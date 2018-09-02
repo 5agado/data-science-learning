fb1f051e7dde1ffc743954abf77ca4e913f72608
sagado <sagado91@yahoo.it>
2018-09-02 22:27:11 +0100

    Initial animation and shrink-to-target-logic

--- graphics/sierpinski_triangle.py
+++ graphics/sierpinski_triangle.py
@@ -1,4 +1,5 @@
 import bpy
+import sys
 from mathutils import Vector
 import numpy as np
 from abc import ABCMeta, abstractmethod
@@ -164,13 +165,15 @@ def cube_replicate_shrink_step(cube: dict, depth: int):
                 else:
                     cube_copy = cube_obj.copy()
                     # TODO fix this
-                    # resize original (all should follow)
+                    # ideally: resize original (all should follow)
+                    # right now: keep original dimensions, so need to keep track of depth
                     cube_copy.scale = Vector((1 / 3**depth, 1 / 3**depth, 1 / 3**depth))
                     cube_copy.location = (x, y, z)
                     new_cube = {
-                        'radius': radius / 3**depth,
+                        'radius': radius / 3,
                         'location': (x, y, z),
-                        'object': cube_copy
+                        'object': cube_copy,
+                        'orgn_scale': cube_copy.scale.copy()
                     }
                     sub_cubes.append(new_cube)
                     scene = bpy.context.scene
@@ -182,6 +185,45 @@ def cube_replicate_shrink_step(cube: dict, depth: int):
     objs = bpy.data.objects
     objs.remove(cube_obj, True)
     return sub_cubes
+
+
+def pyramid_replicate_shrink_step(pyramid: dict, depth: int):
+    radius = pyramid['radius']
+    loc = pyramid['location']
+    pyramid_object = pyramid['object']
+    sub_pyramids = []
+    # amount of shifting for the center of new object
+    center_shift = radius/2
+    # define the five locations for the five new sub-pyramids
+    new_loc_top = (loc[0], loc[1], loc[2] + radius)
+    new_loc_1 = (loc[0] + center_shift, loc[1] + center_shift, loc[2])
+    new_loc_2 = (loc[0] - center_shift, loc[1] + center_shift, loc[2])
+    new_loc_3 = (loc[0] + center_shift, loc[1] - center_shift, loc[2])
+    new_loc_4 = (loc[0] - center_shift, loc[1] - center_shift, loc[2])
+    new_locs = [new_loc_top, new_loc_1, new_loc_2, new_loc_3, new_loc_4]
+    for new_loc in new_locs:
+        pyramid_copy = pyramid_object.copy()
+        # TODO fix this
+        # ideally: resize original (all should follow)
+        # right now: keep original dimensions, so need to keep track of depth
+        pyramid_copy.scale = Vector((1 / 2**depth, 1 / 2**depth, 1 / 2**depth))
+        pyramid_copy.location = new_loc
+        new_cube = {
+            'radius': radius / 2,
+            'location': new_loc,
+            'object': pyramid_copy,
+            'orgn_scale': pyramid_copy.scale.copy()
+        }
+        sub_pyramids.append(new_cube)
+        scene = bpy.context.scene
+        scene.objects.link(pyramid_copy)
+    #scale = 1/3
+    #for v in cube_obj.data.verts:
+    #    v.co *= scale
+    # delete original
+    objs = bpy.data.objects
+    objs.remove(pyramid_object, True)
+    return sub_pyramids
 
 
 # Entry point to call a Recursive-Shrink-Step for a target object
@@ -196,23 +238,106 @@ def rec_shrink(max_depth: int):
 
 
 # Entry point to manage Replication-Shrink for a target object
-def obj_replication(max_depth: int):
+def obj_replication(max_depth: int, use_pyramid=False):
     obj = {
         'radius': 10,
-        'location': (0, 0, 0)
+        'location': (0, 0, 0),
+        'orgn_scale': Vector((1.0, 1.0, 1.0))
     }
 
-    # Cube case
-    cube = Cube(radius=obj['radius'], location=obj['location'])
-    obj['object'] = cube.obj
+    if use_pyramid:
+        # Pyramid case
+        pyramid = Pyramid(radius=obj['radius'], location=obj['location'])
+        obj['object'] = pyramid.obj
+    else:
+        # Cube case
+        cube = Cube(radius=obj['radius'], location=obj['location'])
+        obj['object'] = cube.obj
 
     sub_objs = [obj]
     for i in range(max_depth):
         new_sub_objs = []
         for sub_obj in sub_objs:
-            new_sub_objs.extend(cube_replicate_shrink_step(sub_obj, i+1))
+            if use_pyramid:
+                new_sub_objs.extend(pyramid_replicate_shrink_step(sub_obj, i + 1))
+            else:
+                new_sub_objs.extend(cube_replicate_shrink_step(sub_obj, i + 1))
         sub_objs = new_sub_objs
+    return sub_objs
 
 
-obj_replication(1)
-#rec_shrink(1)
+def scale_objects(object, grid_val, scale_factor=0.2, keyframe=True):
+    obj = object['object']
+    origin_scale = object['orgn_scale']
+    # grid value 1, object should end up with original size
+    if grid_val:
+        # skip all (keyframing too) if already ok, otherwise set original size
+        # TODO this if else check cause some error where the
+        # scaling get scrambled once animation is stopped
+        #if obj.scale == origin_scale:
+        #    return
+        #else:
+        obj.scale = origin_scale.copy()
+    # grid value 0, object should end up scaled
+    else:
+        # skip all (keyframing too) if already ok, otherwise set scaled size
+        #if obj.scale == origin_scale*scale_factor:
+        #    return
+        #else:
+        obj.scale = origin_scale*scale_factor
+    if keyframe:
+        obj.keyframe_insert("scale")
+
+
+def update_grid(objs, target):
+    DIST_LIM = 6.5
+    target_loc = target.location
+    for obj in objs:
+        dist = np.linalg.norm(np.array(target_loc) - np.array(obj['location']))
+        if dist < DIST_LIM:
+            scale_objects(obj, 0)
+        else:
+            scale_objects(obj, 1)
+
+
+# handler called at every frame change
+def frame_handler(scene, objs, target, num_frames_change):
+    TARGET_SPEED = 0.5
+    frame = scene.frame_current
+    n = frame % num_frames_change
+    if n == 0:
+        # move target
+        (x, y, z) = target.location
+        #target.location = (x-TARGET_SPEED, y, z)
+        target.location = (x, y, z - TARGET_SPEED)
+        target.keyframe_insert("location")
+        # update grid
+        update_grid(objs, target)
+
+
+def main(_):
+    # CONSTANTS
+    num_frames_change = 2
+
+    #bpy.ops.mesh.primitive_ico_sphere_add(
+    #                    subdivisions = 4,
+    #                    size = 1,
+    #                    location = (25, 0, 0))
+    bpy.ops.mesh.primitive_ico_sphere_add(
+                       subdivisions = 4,
+                       size = 0.3,
+                       location = (0, 0, 30))
+
+    target = bpy.context.scene.objects.active
+    target.keyframe_insert("location")
+
+    objs = obj_replication(4, use_pyramid=True)
+    # rec_shrink(1)
+    update_grid(objs, target)
+
+    bpy.app.handlers.frame_change_pre.clear()
+    bpy.app.handlers.frame_change_pre.append(lambda x: frame_handler(x, objs, target, num_frames_change))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
