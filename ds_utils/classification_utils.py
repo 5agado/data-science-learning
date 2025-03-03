7ad53f0dee1dcc405fa2db25cc111ea13046e698
Alex Martinelli <alex.martinelli@zalando.ie>
2025-03-03 09:44:31 +0000

    Cleanup clf and plot utils

--- ds_utils/classification_utils.py
+++ ds_utils/classification_utils.py
@@ -1,86 +1,157 @@
-from sklearn.metrics import accuracy_score, log_loss
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn.model_selection import train_test_split
-
-from sklearn import linear_model
-from sklearn import tree
-from sklearn import neighbors
-from sklearn import ensemble
-from sklearn import svm
-from sklearn import gaussian_process
-from sklearn import naive_bayes
-from sklearn import neural_network
-from sklearn import model_selection
-
+import json
+import logging
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Dict, List, Any
+from ast import literal_eval
 import numpy as np
-import time
-
-def get_classifiers():
-    clfs = {}
-        #clfs['bag'] = {
-        #    'clf': ensemble.BaggingClassifier(neighbors.KNeighborsClassifier(), max_samples=0.5,
-        #                                      max_features=0.5), 'name': "BaggingClassifier"}
-        # clfs['mlp'] = {'clf': neural_network.MLPClassifier(hidden_layer_sizes=(100,100,100), alpha=1e-5, solver='lbfgs', max_iter=500), 'name': 'MultilayerPerceptron'}
-
-    clfs['logreg'] = {'clf': linear_model.LogisticRegression(),
-                      'params': {'C': [(2**x) for x in np.arange(-5, 15, step=3)]}}
-    clfs['sgd'] = {'clf': linear_model.SGDClassifier(),
-                   'params': {'loss': ['perceptron'], 'alpha': 10 ** np.random.uniform(-6, 1)}}
+import joblib
+import sklearn
+from sklearn import metrics
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
+import optuna
 
 
-    clfs['knc'] = {'clf':neighbors.KNeighborsClassifier(), 'params': {'n_neighbors':np.arange(3, 15)}}
-    clfs['rfc'] = {'clf':ensemble.RandomForestClassifier(), 'params':{'n_estimators':np.arange(64, 1024, step=64)}}
-    clfs['svc'] = {'clf': svm.SVC(), 'params': {'kernel':['linear', 'sigmoid', 'poly', 'rbf'], 'gamma':np.linspace(0.0,2.0,num=21),'C': np.linspace(0.5,1.5,num=11)}}
-    clfs['abc'] = {'clf': ensemble.AdaBoostClassifier(), 'params': {'n_estimators': np.arange(64, 1024, step=64)}}
-    clfs['gbc'] = {'clf': ensemble.GradientBoostingClassifier(), 'params': {'n_estimators': np.arange(64, 1024, step=64)}}
+@dataclass
+class DataEntry:
+    value: np.ndarray
+    label: str
 
-    clfs['gauss_class'] = {'clf': gaussian_process.GaussianProcessClassifier(), 'params': {}}
-    clfs['gauss_nb'] = {'clf': naive_bayes.GaussianNB(), 'params': {}}
 
-    #LinearDiscriminantAnalysis(),
-    #QuadraticDiscriminantAnalysis()
+def train_clf(dataset: Dict[str, Dict[str, DataEntry]], clf_params: Dict[str, Any]):
+    # train classifier with target params
+    estimator = clf_params.pop('estimator')
+    if estimator == "svc":
+        clf = SVC(**clf_params)
+    elif estimator == "mlp":
+        # convert str from optuna to expected sklearn value
+        clf_params['hidden_layer_sizes'] = literal_eval(clf_params['hidden_layer_sizes'])
+        clf_params['solver'] = clf_params.pop('solver_mpl')
+        clf = MLPClassifier(**clf_params)
+    elif estimator == 'logres':
+        clf_params['solver'] = clf_params.pop('solver_logres')
+        clf = LogisticRegression(**clf_params)
+    else:
+        raise ValueError(f'Unknown estimator: {estimator}')
 
-    return clfs
-
-def test_classifier(clf, X_train, y_train, X_test, y_test):
-    # Train model
-    start = time.time()
+    X_train = np.array([entry.value for entry in dataset['train'].values()])
+    y_train = np.array([entry.label for entry in dataset['train'].values()])
     clf.fit(X_train, y_train)
-    end = time.time()
-
-    # Evaluate quality of model on test data
-    predictions = clf.predict(X_test)
-    accuracy = accuracy_score(y_test, predictions)
-
-    pred_probs = clf.predict_proba(X_test)
-    loss = log_loss(y_test, pred_probs)
-
-    return end - start, accuracy, loss
-
-def test_classifiers(clfs, data, labels):
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(data, labels,
-                                                        test_size=0.33, random_state=42)
-
-    for clf in clfs:
-        time, accuracy, loss = test_classifier(clf, X_train, y_train, X_test,
-                                                                    y_test)
-        print("=" * 30)
-        print(clf.__class__.__name__)
-        print("Time: {:.4f}".format(time))
-        print("Accuracy: {:.4%}".format(accuracy))
-        print("Log Loss: {}".format(loss))
-
-def grid_search(clfs, X_train, y_train):
-    for key, clf in clfs.items():
-        print(key)
-        grid_clf = model_selection.GridSearchCV(clf['clf'], clf['params'])
-        grid_clf.fit(X_train, y_train)
-        clfs[key]['best_params'] = grid_clf.best_params_
-        clfs[key]['max_test_score'] = max(grid_clf.cv_results_['mean_test_score'])
+    return clf
 
 
-#imp = clf.feature_importances_
-#names = X_train.columns
-#res = pd.DataFrame({'name':names, 'importance':imp})
-#res.sort_values(by=['importance'], ascending=False)[:31]
+def eval_clf(clf, dataset: Dict[str, Dict[str, DataEntry]], class_map: List[str],
+             target_split='test') -> Dict[str, Any]:
+    raw_preds, y_test_oh, _ = clf_predict(clf, dataset, target_split, class_map, True)
+    clf_oh_preds = raw_preds >= raw_preds.max(axis=1).reshape(-1, 1)
+
+    accuracy = metrics.accuracy_score(y_test_oh, clf_oh_preds)
+    p, r, fscore, support = metrics.precision_recall_fscore_support(y_test_oh, clf_oh_preds)
+    eval_metrics = {
+        'accuracy': accuracy,
+        'precision': p,
+        'recall': r,
+        'fscore': fscore,
+        'support': support
+    }
+    return eval_metrics
+
+
+def clf_predict(clf, dataset: Dict[str, Dict[str, DataEntry]], target_split: str, class_map, one_hot_encoding=False):
+    keys = list(dataset[target_split].keys())
+    x = np.array([entry.value for entry in dataset[target_split].values()])
+    y = np.array([entry.label for entry in dataset[target_split].values()])
+    if one_hot_encoding:
+        y = get_one_hot_encoding(y, class_map)
+    raw_preds = clf.predict_proba(x)
+    return raw_preds, y, keys
+
+
+def clf_objective(trial, max_iter, dataset: Dict[str, Dict[str, DataEntry]],
+                  class_map: List[str]) -> float:
+    """Optuna objective function that optimises classifier accuracy score."""
+    # Parameter tuning
+    estimator = trial.suggest_categorical("estimator", ["svc", "mlp", "logres"])
+    trial.set_user_attr("random_state", 0)
+    trial.set_user_attr("max_iter", max_iter)
+    if estimator == "svc":
+        kernel = trial.suggest_categorical("kernel", ["linear", "poly", "rbf", "sigmoid"])
+        trial.set_user_attr("probability", True)
+        trial.suggest_float("C", 1e0, 1e3, log=True),
+        if kernel == "poly":
+            trial.suggest_int("degree", 2, 4)
+        if kernel in ["poly", "rbf", "sigmoid"]:
+            trial.suggest_categorical("gamma", ['scale', 'auto'])
+    elif estimator == "mlp":
+        trial.suggest_categorical("solver_mpl", ["adam", "lbfgs", "sgd"])
+        trial.suggest_categorical("learning_rate", ["constant", "invscaling", "adaptive"])
+        trial.suggest_categorical("hidden_layer_sizes", ["(100,)", "(256, 128)"])
+    elif estimator == "logres":
+        trial.suggest_categorical("solver_logres", ["newton-cg", "lbfgs", "sag", "saga"])
+        trial.suggest_float("C", 1e-3, 1e3, log=True)
+
+    # for trials repeated (same params), return already computed results
+    complete_trials = trial.study.get_trials(deepcopy=False, states=(optuna.trial.TrialState.COMPLETE,))
+    for t in complete_trials[::-1]:
+        if trial.params == t.params:
+            return t.value
+
+    clf = train_clf(dataset, clf_params={**trial.params, **trial.user_attrs})
+    eval_metrics = eval_clf(clf, dataset, class_map, target_split='valid')
+    return eval_metrics['accuracy']
+
+
+def tune_train_eval_clf(num_trials, max_iter, dataset: Dict[str, Dict[str, DataEntry]],
+                        class_map: List[str]):
+    # Perform hyper-parameter tuning using optuna
+    logging.info(f'Running Optuna study...')
+    clf_study = optuna.create_study(study_name='clf_study', direction='maximize')
+    clf_study.optimize(
+        lambda trial: clf_objective(trial, max_iter, dataset, class_map), n_trials=num_trials)
+    display_optuna_study_results(clf_study, logging)
+
+    # Train classifier
+    logging.info('Training shallow classifier...')
+    clf = train_clf(dataset, {**clf_study.best_trial.user_attrs, **clf_study.best_trial.params})
+
+    # Evaluate classifier
+    logging.info('Evaluating shallow classifier...')
+    eval_metrics = eval_clf(clf, dataset, class_map, target_split='test')
+
+    return clf, clf_study, eval_metrics
+
+
+def display_optuna_study_results(study, logger):
+    logger.info("Optuna study results:")
+    logger.info(f"Number of finished trials: {len(study.trials)}")
+    logger.info("Best trial:")
+    trial = study.best_trial
+    logger.info(f"score: {trial.value}")
+    logger.info("Params: ")
+    for key, value in trial.params.items():
+        logger.info(f"{key}: {value}")
+
+
+def export_model(clf, model_dir: Path, model_name, class_map, model_config=None):
+    logging.info(f'Writing model artifacts to : {model_dir}')
+    model_dir.mkdir(exist_ok=False, parents=True)
+    joblib.dump(clf, f'{model_dir}/model.joblib')
+
+    # create and save model config
+    model_config = model_config or {}
+    model_config.update({
+        "model_name": model_name,
+        "class_map": class_map
+    })
+    joblib.dump(model_config, f'{model_dir}/config.joblib')
+    with open(f'{model_dir}/config.json', 'w') as outfile:
+        json.dump(model_config, outfile, indent=2)
+
+
+def get_one_hot_encoding(values, class_map: List[str]):
+    # Transform list of class-indices to one-hot encoding
+    one_hot = np.zeros((len(values), len(class_map)+1))
+    one_hot[np.arange(len(values)), values] = 1
+    return one_hot[:, :-1]
--- ds_utils/plot_utils.py
+++ ds_utils/plot_utils.py
@@ -1,30 +1,30 @@
-import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
-from matplotlib import animation
 import numpy as np
 
 
-def plot_sample_imgs(get_imgs_fun, img_shape, plot_side=5, savepath=None, cmap='gray'):
+def plot_sample_images(nrows, ncols, get_image_fun, get_text_fun=None, figsize=5, savepath=None):
     """
-    Generate visual samples and plot on a grid
-    :param get_imgs_fun: function that given a int return a corresponding number of generated samples
-    :param img_shape: shape of image to plot
-    :param plot_side: samples per row (and column). Generated plot_side x plot_side samples
-    :param savepath: if given, save plot to such filepath, otherwise show plot
-    :param cmap: matplotlib specific cmap to use for the plot
+    Plot a grid of images
+    :param nrows: Number of rows
+    :param ncols: Number of columns
+    :param get_image_fun: fun that returns an image given an index
+    :param get_text_fun: optional fun that returns text given an index
     """
-    fig, axarr = plt.subplots(plot_side, plot_side)
-    #dpi = 100
-    #fig, ax = plt.subplots(dpi=dpi, figsize=(img_shape[0] / dpi, img_shape[1] / dpi))
-    #fig.subplots_adjust(left=0, right=1, bottom=0, top=1)  # hspace=0.4, wspace=0.4
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(ncols*figsize,nrows*figsize))
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)  # hspace=0.4, wspace=0.4
 
-    samples = get_imgs_fun(plot_side*plot_side)
-    for row in range(plot_side):
-        for col in range(plot_side):
-            axarr[row, col].imshow(samples[plot_side*row+col].reshape(img_shape), cmap=cmap)
-            axarr[row, col].set_title('')
-            axarr[row, col].axis('off')
+    img_count = 0
+    for row in range(nrows):
+        for col in range(ncols):
+            ax = axes[row, col]
+            ax.imshow(get_image_fun(img_count))
+            ax.axis("off")
+            ax.set_aspect('equal')
+            if get_text_fun:
+                text = get_text_fun(img_count)
+                ax.text(0.5, -0.1, text, size=12, ha="center", transform=ax.transAxes)
+            img_count += 1
 
     # Save figure if savepath is provided
     if savepath:
